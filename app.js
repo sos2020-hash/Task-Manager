29ae4711c24c932e05cd7e5a80b891cb96eb58d3
sos2020-hash <qweiop525@gmail.com>
2021-09-28 21:40:07 +0800

    add model middleware errorhandler

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/notfound");
+const errorHandlerMiddleware = require("./middleware/errorhandler");
 require("dotenv").config();
 //middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager");
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 const port = 3000;
 
 const start = async () => {
